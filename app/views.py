2986ccf529707e2d3bcc3f0f6e6543c07202fcb7
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-24 23:05:04 -0500

    Added forms to website

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory, request
 from app import app
 
 @app.route('/')
@@ -8,3 +8,13 @@ def index():
     return render_template('index.html',
                            title='Home',
                            user=user)
+
+@app.route('/form', methods=['POST'])
+def form():
+    print("PASSWORD ENTERED: {}".format(request.form["password"]))
+    return render_template('form.html')
+    
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
